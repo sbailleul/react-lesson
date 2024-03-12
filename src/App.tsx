8bb81cf4cabba195e6f1282d4ab153e78688d4bc
sbailleul <bailleul.sacha@gmail.com>
2024-03-12 10:48:08 +0000

    feat(employee): Render two kinds of employees

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import "@/App.scss";
 import { Employee } from "@/office/Employee";
-import  michael_scott from '@/assets/employees/michael_scott.jpg'
+import michael_scott from "@/assets/employees/michael_scott.jpg";
+import pam_beesly from "@/assets/employees/pam_beesly.jpg";
 export function App() {
   return (
-    <Employee
-      firstname="Michael"
-      lastname="Scott"
-      img={michael_scott}
-    ></Employee>
+    <div className="d-flex">
+      <Employee
+        firstName="Michael"
+        lastName="Scott"
+        position="RegionalManager"
+        img={michael_scott}
+      ></Employee>
+      <Employee
+        firstName="Pam"
+        lastName="Beesly"
+        position="Receptionist"
+        img={pam_beesly}
+      ></Employee>
+    </div>
   );
 }
