5232b1b056a59ed244357f367d6cb30d4afdf47f
sbailleul <bailleul.sacha@gmail.com>
2024-04-19 07:20:56 +0000

    feat(todosCounter): Increment counter

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -6,12 +6,16 @@ export type TodoProps = {
   status: boolean;
   onDelete: () => void;
 };
+
+
 export function Todo({ title, description, status, onDelete }: TodoProps) {
 // let text =title;
   const [text, setText] = useState<string>(title);
+  const [counter, setCounter] = useState(0);
   return (
     <div>
       <h1>{text}</h1>
+      <h2>Nb de click : {counter}</h2>
       <p>{description}</p>
       {status ? <span>Done</span> : <span>Todo</span>}
       <button
@@ -22,6 +26,12 @@ export function Todo({ title, description, status, onDelete }: TodoProps) {
       >
         Majuscule
       </button>
+      <button
+        onClick={() => {
+         setCounter((counter) =>  counter +  1 )
+         
+        }}
+      >Increment counter</button>
       <div onClick={() => alert("div clicked")}>
         <button
           className="btn btn-danger"
