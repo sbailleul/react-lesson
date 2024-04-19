6d826de019df80a2789c354004cba4cf263ab1ef
sbailleul <bailleul.sacha@gmail.com>
2024-04-19 13:01:12 +0000

    feat(counter): Increment counter

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 type TodoProps = {
   title: string;
   description: string;
@@ -5,10 +7,19 @@ type TodoProps = {
   onDelete: () => void;
 };
 export function Todo({ title, description, status, onDelete }: TodoProps) {
+  const [counter, setCounter] = useState(0);
   return (
     <div>
       <h1>{title}</h1>
+      <h2>Count : {counter}</h2>
       <p>{description}</p>
+      <button
+        onClick={() => {
+          setCounter(counter + 1);
+        }}
+      >
+        Increment
+      </button>
       <span>
         <span>{status ? "Done" : "Todo"}</span>
       </span>
