134eb9026ea7fd5cf11d89e99ab7288bb18c5801
sbailleul <bailleul.sacha@gmail.com>
2024-04-19 06:55:48 +0000

    feat(todoText): use state to uppercase text

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,21 +1,38 @@
+import { useState } from "react";
+
 export type TodoProps = {
   title: string;
   description: string;
   status: boolean;
-  onDelete: () => void
+  onDelete: () => void;
 };
 export function Todo({ title, description, status, onDelete }: TodoProps) {
+// let text =title;
+  const [text, setText] = useState<string>(title);
   return (
     <div>
-      <h1>{title}</h1>
+      <h1>{text}</h1>
       <p>{description}</p>
       {status ? <span>Done</span> : <span>Todo</span>}
       <button
-        className="btn btn-danger"
-        onClick={onDelete}
+        onClick={() => {
+            // text = text.toUpperCase()
+          setText(text.toUpperCase());
+        }}
       >
-        Supprimer
+        Majuscule
       </button>
+      <div onClick={() => alert("div clicked")}>
+        <button
+          className="btn btn-danger"
+          onClick={() => {
+            onDelete();
+          }}
+        >
+          Supprimer
+        </button>
+      </div>
+
       {/* {status &&  <span>Done</span>  }
         {!status &&  <span>Todo</span>  } */}
     </div>
