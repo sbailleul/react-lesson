b8387c715b69dc2c22e723d507bcb5bf53eb5bc4
sbailleul <bailleul.sacha@gmail.com>
2024-04-19 06:22:47 +0000

    feat(todo): Click

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,16 +1,26 @@
-export type TodoProps = {title: string, description: string, status: boolean}
-export function Todo({title, description, status}: TodoProps){
-    // let statusText: string | undefined;
-    // if(status){
-    //     statusText= "Done";
-    // }else{
-    //     statusText= "Todo"
-    // }
-    return (<div>
-        <h1>{title}</h1>
-        <p>{description}</p>
-        {status ? <span>Done</span>  : <span>Todo</span>}
-        {/* {status &&  <span>Done</span>  }
+export type TodoProps = { title: string; description: string; status: boolean };
+export function Todo({ title, description, status }: TodoProps) {
+  // let statusText: string | undefined;
+  // if(status){
+  //     statusText= "Done";
+  // }else{
+  //     statusText= "Todo"
+  // }
+  return (
+    <div>
+      <h1>{title}</h1>
+      <p>{description}</p>
+      {status ? <span>Done</span> : <span>Todo</span>}
+      <button
+        className="btn btn-outline-info"
+        onClick={() => {
+          alert("Todo clicked");
+        }}
+      >
+        Afficher alert
+      </button>
+      {/* {status &&  <span>Done</span>  }
         {!status &&  <span>Todo</span>  } */}
-    </div>)
+    </div>
+  );
 }
