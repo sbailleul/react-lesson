0d68f0d00c0a5b05e40d26484363359c1828aa26
sbailleul <bailleul.sacha@gmail.com>
2024-04-19 08:02:15 +0000

    feat(saveTodos): Save todos

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -3,12 +3,36 @@ import { useState } from "react";
 
 type Todo = { title: string; description: string; status: boolean };
 export type TodoListProps = { todos: Todo[] };
+
+function delay() {
+  return new Promise((resolve) => {
+    setTimeout(resolve, 3000);
+  });
+}
+type LoadingStatus = 'idle' | 'pending' | 'success'
 export function TodoList({ todos }: TodoListProps) {
   const [title, setTitle] = useState("Default title");
+  const [loadingStatus, setLoadingStatus] = useState<LoadingStatus>('idle');
+  const handleSave = async () => {
+    setLoadingStatus('pending');
+    await delay()
+    setLoadingStatus('success')
+  }
   return (
     <div>
       <h1>{title}</h1>
-      <input className="form-control" placeholder="The todo title" value={title} type="text" onChange={(e) => setTitle(e.target.value)} />
+      <button className="btn btn-success" onClick={handleSave}>
+        Save
+      </button>
+      <input
+        className="form-control"
+        placeholder="The todo title"
+        value={title}
+        type="text"
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      {loadingStatus === 'pending' && <h3 className="bg-info">Pending todos : {todos.length} </h3>}
+      {loadingStatus === 'success' && <h3 className="bg-success">Saved todos : {todos.length} </h3>}
       {todos.map((todo) => {
         return (
           <Todo
