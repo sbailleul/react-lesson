4a2bc4e543180ae7fd375e0ef29a45b3d7e9b46d
sbailleul <bailleul.sacha@gmail.com>
2024-04-19 07:53:12 +0000

    feat(todosListTitle): Edit todo title

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -7,32 +7,33 @@ export type TodoProps = {
   onDelete: () => void;
 };
 
-
 export function Todo({ title, description, status, onDelete }: TodoProps) {
-// let text =title;
   const [text, setText] = useState<string>(title);
   const [counter, setCounter] = useState(0);
   return (
-    <div>
+    <div className="card">
       <h1>{text}</h1>
       <h2>Nb de click : {counter}</h2>
       <p>{description}</p>
       {status ? <span>Done</span> : <span>Todo</span>}
-      <button
-        onClick={() => {
+      <div className="d-flex">
+        <button
+          className="btn btn-outline-info"
+          onClick={() => {
             // text = text.toUpperCase()
-          setText(text.toUpperCase());
-        }}
-      >
-        Majuscule
-      </button>
-      <button
-        onClick={() => {
-         setCounter((counter) =>  counter +  1 )
-         
-        }}
-      >Increment counter</button>
-      <div onClick={() => alert("div clicked")}>
+            setText(text.toUpperCase());
+          }}
+        >
+          Majuscule
+        </button>
+        <button
+          className="btn btn-outline-info"
+          onClick={() => {
+            setCounter((counter) => counter + 1);
+          }}
+        >
+          Increment counter
+        </button>
         <button
           className="btn btn-danger"
           onClick={() => {
@@ -42,7 +43,6 @@ export function Todo({ title, description, status, onDelete }: TodoProps) {
           Supprimer
         </button>
       </div>
-
       {/* {status &&  <span>Done</span>  }
         {!status &&  <span>Todo</span>  } */}
     </div>
--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,11 +1,24 @@
 import { Todo } from "@/Todo";
+import { useState } from "react";
 
 type Todo = { title: string; description: string; status: boolean };
 export type TodoListProps = { todos: Todo[] };
 export function TodoList({ todos }: TodoListProps) {
-  return <div>
-    {todos.map(todo => {
-        return <Todo title={todo.title} description={todo.description} status={todo.status} onDelete={() =>alert(`todo deleted ${todo.title}`) }/>
-    })}
-  </div>;
+  const [title, setTitle] = useState("Default title");
+  return (
+    <div>
+      <h1>{title}</h1>
+      <input className="form-control" placeholder="The todo title" value={title} type="text" onChange={(e) => setTitle(e.target.value)} />
+      {todos.map((todo) => {
+        return (
+          <Todo
+            title={todo.title}
+            description={todo.description}
+            status={todo.status}
+            onDelete={() => alert(`todo deleted ${todo.title}`)}
+          />
+        );
+      })}
+    </div>
+  );
 }
