10ef0996d7b40cd66710ccde40639bf46d3b9901
sbailleul <bailleul.sacha@gmail.com>
2024-07-01 14:39:04 +0200

    feat(reducer): Use reducer

--- src/EditableTodo.tsx
+++ src/EditableTodo.tsx
@@ -1,41 +1,57 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
 
 export type Todo = { title: string; description: string; status: boolean };
+type UpdateTitleAction = { type: "UPDATE_TITLE"; title: string };
+type UpdateDescriptionAction = {
+  type: "UPDATE_DESCRIPTION";
+  description: string;
+};
+type UpdateStatusAction = { type: "UPDATE_STATUS"; status: boolean };
 
-type EditableTodoProps = {onSave: () => void}
-export function EditableTodo({onSave}: EditableTodoProps) {
-  const [todo, setTodo] = useState<Todo>({
+function todoReducer(
+  todo: Todo,
+  action: UpdateTitleAction | UpdateDescriptionAction | UpdateStatusAction
+): Todo {
+  switch (action.type) {
+    case "UPDATE_TITLE":
+      return { ...todo, title: action.title };
+    case "UPDATE_DESCRIPTION":
+      return { ...todo, description: action.description };
+    case "UPDATE_STATUS":
+      return { ...todo, status: action.status };
+  }
+}
+type EditableTodoProps = { onSave: () => void };
+export function EditableTodo({ onSave }: EditableTodoProps) {
+  const [todo, dispatch] = useReducer(todoReducer, {
     title: "",
     description: "",
     status: false,
   });
-  const canSaveTodo = !!todo.description && !!todo.title;
 
+  const canSaveTodo = !!todo.description && !!todo.title;
+  console.log(todo)
   return (
     <div className="card bg-success">
       <input
         type="text"
         onChange={(e) => {
-          setTodo({ ...todo, title: e.target.value });
+          dispatch({ type: "UPDATE_TITLE", title: e.target.value });
         }}
       />
       <input
         type="text"
         onChange={(e) => {
-          setTodo({ ...todo, description: e.target.value });
+          dispatch({ type: "UPDATE_DESCRIPTION", description: e.target.value });
         }}
       />
       <input
         type="checkbox"
         onChange={() => {
-          setTodo({ ...todo, status: !todo.status });
+          dispatch({ type: "UPDATE_STATUS", status: !todo.status });
         }}
       />
-      <button
-        className="btn"
-        disabled={!canSaveTodo}
-        onClick={onSave}
-      >
+      <button className="btn" disabled={!canSaveTodo} onClick={onSave}>
         Save
       </button>
     </div>
