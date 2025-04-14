ea4a1eb937b6057327a1e932ecf8a306e0825a68
sbailleul <bailleul.sacha@gmail.com>
2025-04-14 16:17:38 +0200

    feat(student-form): Add student

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,10 @@
 import "@/App.scss";
-import dwight_schrute from "@/assets/employees/dwight_schrute.jpg";
-import jim_halper from "@/assets/employees/jim_halper.png";
-import michael_scott from "@/assets/employees/michael_scott.jpg";
-import pam_beesly from "@/assets/employees/pam_beesly.jpg";
-import { Employees, IdentifiedEmployee } from "@/office/Employees";
-
-// On initialise la liste des employés en dehors du composant pour éviter de récréer cette variable à chaque fois que le composant App est rendu
-const employees: IdentifiedEmployee[] = [
-  {
-    id: "uniq_1",
-    firstName: "Michael",
-    lastName: "Scott",
-    position: "RegionalManager",
-    img: michael_scott,
-  },
-  {
-    id: "uniq_2",
-    firstName: "Pam",
-    lastName: "Beesly",
-    position: "Receptionist",
-    img: pam_beesly,
-  },
-  {
-    id: "uniq_3",
-    firstName: "Dwight",
-    lastName: "Schrute",
-    position: "AssistantToTheRegionalManager",
-    img: dwight_schrute,
-  },
-  {
-    id: "uniq_4",
-    firstName: "Jim",
-    lastName: "Halper",
-    position: "SaleRepresentative",
-    img: jim_halper,
-  },
-];
+import {Form} from "@/tic-tac-toe/Form";
 
 export function App() {
   return (
     <div className="d-flex">
-      <Employees employees={employees} />
+      <Form title="AL1" fired={false}  />
     </div>
   );
 }
---
+++ src/tic-tac-toe/Form.tsx
@@ -0,0 +1,54 @@
+import { useEffect, useRef, useState } from "react";
+
+type Props = {
+  title: string;
+  fired: boolean;
+};
+const initialStudents = [
+  { id: "a", name: "Ylan" },
+  { id: "b", name: "Mathias" },
+  { id: "c", name: "Nicolas" },
+  { id: "d", name: "Elias" },
+];
+type Pokemon = { name: string };
+export function Form({ title, fired }: Props) {
+  const [students, setStudents] = useState(initialStudents);
+  const [newStudentName, setNewStudentName] = useState("François");
+  if (fired) {
+    return undefined;
+  }
+  return (
+    <>
+      <div className="flex flex-column">
+        <h1>Title : {title} </h1>
+        <div>
+          <input
+            id="student-name"
+            placeholder="Type your new student name"
+            onChange={(e) => {
+              setNewStudentName(e.target.value);
+            }}
+            defaultValue={newStudentName}
+          />
+          <button
+            onClick={() => {
+              setNewStudentName("");
+              students.push();
+              setStudents([{id: newStudentName, name: newStudentName}, ...students]);
+            }}
+          >
+            Add student
+          </button>
+        </div>
+
+        {students.map((student) => {
+          return (
+            <div key={student.id} className="d-flex w-full">
+              {student.name}
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+}
