cfb600a448831503c21502a89d68f02e490fb4e3
sbailleul <bailleul.sacha@gmail.com>
2025-07-04 18:09:28 +0200

    chore(ci-cd): Use base url

--- src/main.tsx
+++ src/main.tsx
@@ -21,8 +21,8 @@ async function enableMocking() {
   // `worker.start()` returns a Promise that resolves
   // once the Service Worker is up and ready to intercept requests.
   return worker.start({
-    findWorker: (scriptUrl) => {
-      return scriptUrl.includes("mockServiceWorker");
+    serviceWorker: {
+      url: import.meta.env.BASE_URL + "/mockServiceWorker.js",
     },
   });
 }
