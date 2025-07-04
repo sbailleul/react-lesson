e489bc6582dbd5ea15bc73fce5e5712dd26d9a87
sbailleul <bailleul.sacha@gmail.com>
2025-07-04 18:19:11 +0200

    fix(routing): Add link

--- src/features/students-managment/components/Navbar.tsx
+++ src/features/students-managment/components/Navbar.tsx
@@ -1,13 +1,21 @@
+import { Link } from "react-router-dom";
+
 export function Navbar() {
   return (
     <nav className="navbar navbar-expand navbar-light bg-light mb-3">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">My App</a>
-          <div className="navbar-nav">
-            <a className="nav-link" href="/">Home</a>
-            <a className="nav-link" href="/students">Students</a>
-          </div>
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          My App
+        </a>
+        <div className="navbar-nav">
+          <Link className="nav-link" to="/">
+            Home
+          </Link>
+          <Link className="nav-link" to="/students">
+            Students
+          </Link>
         </div>
-      </nav>
-  )
+      </div>
+    </nav>
+  );
 }
